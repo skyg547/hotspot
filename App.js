220ea4e44c2f9e28620dd715863c4093139715f1
skyg547 <skyg547@gmail.com>
2020-09-18 23:35:08 +0900

    제스처 핸들러 추가

--- App.js
+++ App.js
@@ -7,17 +7,15 @@
  */
 
 import React from 'react';
-import {  SafeAreaView,  StyleSheet,  ScrollView,  View,  Text,  StatusBar,} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './nav/AppNavigator';
-import {  Header,  LearnMoreLinks,  Colors,  DebugInstructions,  ReloadInstructions,} from 'react-native/Libraries/NewAppScreen';
-import { fromPairs } from 'lodash';
+import 'react-native-gesture-handler';
 
 const App: () => React$Node = () => {
   return (
     <>
       <NavigationContainer>
-        <AppNavigator/>
+        <AppNavigator />
       </NavigationContainer>
     </>
   );
